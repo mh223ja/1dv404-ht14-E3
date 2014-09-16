7f9ff45270a355aa43b58f1413df352f55b2584d
mh223ja <mh223ja@student.lnu.se>
2014-09-16 22:15:49 +0200

    lab2a

---
+++ Lab2a.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //establish rows
+            for (int row = 0; row<25; row++)
+            //designate colors
+            {
+                switch (row % 3)
+
+                    {
+                    case 1: 
+                        Console.ForegroundColor=ConsoleColor.Yellow;
+                        break;
+                    case 2:
+                        Console.ForegroundColor=ConsoleColor.Magenta;
+                        break;
+                    case 3:
+                        Console.ForegroundColor=ConsoleColor.Green;
+                        break;
+ }
+//space before row
+                if ((row %2) == 0) 
+                     {Console.Write(""); }
+
+                for (int column = 0; column <39; column++)
+                 {Console.Write ("* "); }
+                Console.ResetColor ();
+                Console.WriteLine();
+                }
+            //write asterix
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab 2.1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //establish loop
+            for (int row = 0; row < 25; row++)
+          //different colors for rows
+{ 
+            switch  (row %3)
+            { 
+
+            case"0" :
+                Console.BackgroundColor=ConsoleColor.Yellow;
+                break;
+            case "1": 
+                Console.BackgroundColor=ConsoleColor.Magenta;
+                break;
+            case "2":
+                Console.BackgroundColor=ConsoleColor.Green;
+                break;
+                     }
+                //space every other line
+            if ((row  % 2)=0)
+               
+               { Console.Write(""); }
+                for (int column = 1; column<39; column++)
+                    { Console.Write("*"); }
+                 }
+        }
+    }
+}
