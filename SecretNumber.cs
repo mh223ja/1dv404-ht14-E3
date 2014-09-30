af2ddde41c45780714f7cbedf346b5e2087942d9
mh223ja <mh223ja@student.lnu.se>
2014-09-30 21:38:14 +0200

    Working Lab 2.1

--- SecretNumber.cs
+++ SecretNumber.cs
@@ -13,55 +13,61 @@ namespace _1DV402.S2.L1A
         private int _number;
         public const int MaxNumberOfGuesses = 7;
 
-
+        //Initialize (public)
         public void Initialize()
         {
-            _count = 0;
-            Random random = new Random();
-            _number = random.Next(1, 101);
+            _count = 0; //set count
+
+            Random random = new Random(); //set random number
+            _number = random.Next(1, 101); //set at 101 to make sure limit is 100
         }
 
-        public bool MakeGuess(int number)
+        public bool MakeGuess(int number) //must be bool for MakeGuess with int
         {
 
-          if (number < 1 || number > 100)
+            if (number < 1 || number > 100)
             {
-                throw new ArgumentOutOfRangeException();
+                throw new ArgumentOutOfRangeException(); //catch numbers that are outside of range
             }
 
-          else
-          {
-            if (_count < MaxNumberOfGuesses)
-            { 
-                if (number < _number)
+            else
+            {
+                while (_count < MaxNumberOfGuesses) //if number is under 7
                 {
-                    Console.WriteLine("{0} är för lågt. Du har {0} gissningar kvar.", number, (6 - _count));
-                }
-
-                else (number > _number)
+                    if (number == _number)
                     {
-                        Console.WriteLine("{0} är för högt. Du har {0} gissningar kvar.", number, (6 - _count));
+                        Console.WriteLine("RÄTT GISSAT! Du klarade det på {0} gissningar", (1 + _count));
+                        return true;
                     }
-                    
-                    
-                else (number == _number)
-                        {
-                            Console.WriteLine("RÄTT GISSAT! Du klarade det på {0} gissningar", (6 + _count));
-                        }
-                    
-                else   if (_count == 6)
+
+                    else
                     {
-                        Console.WriteLine("Ingen fler gissningar. Det hemliga talet var {0}.", _number);
+                        if (number < _number)  //messages - too low, and how many guesses left
+                        {
+                            Console.WriteLine("{0} är för lågt. Du har {1} gissningar kvar.", number, (6 - _count));
+                        }
+                        //guesses should be 6 - count to account for 0
+
+                        else if (number > _number)
+                        {
+                            Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar.", number, (6 - _count));
+                        }
+
+
                     }
                     _count++;
+                    if (_count > 6)
+                        Console.WriteLine("Ingen fler gissningar. Det hemliga talet var {0}.", _number);
+
+
                     return false;
                 }
-        
-        else
-    {
-        throw new ApplicationException ();
-    }
-             }
+
+
+                {
+                    throw new ApplicationException();
+                }
+            }
         }
         public SecretNumber()
         {
