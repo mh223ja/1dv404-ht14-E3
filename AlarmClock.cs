0f68e796e80b34243c68dadb701f266facf621c0
mh223ja <mh223ja@student.lnu.se>
2014-10-06 20:31:21 +0200

    startinglab2.2

---
+++ AlarmClock.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    class AlarmClock
+    {
+        
+        
+            int _alarmHour;
+            int _alarmMinute;
+            int _hour;
+            int _minute;
+
+           
+            }
+            public int AlarmHour ()
+            {
+                get {
+                    return _alarmHour;
+                }
+                set
+                {
+                    _alarmHour = value;
+                }
+              
+
+            
+
+                if (alarmHour < 0 ||> 23)
+                {
+                    throw new ArgumentException();
+
+                }
+               
+              
+            }
+
+
+
+        }
+    }
+
