f703c288c4052477871142e7fc9d7a514789badb
mh223ja <mh223ja@student.lnu.se>
2014-10-22 21:13:27 +0200

    startingover

---
+++ 3-1-recept-pa-fil-master/FiledRecipes/FiledRecipes/Views/RecipeView.cs
@@ -0,0 +1,21 @@
+﻿using FiledRecipes.Domain;
+using FiledRecipes.App.Mvp;
+using FiledRecipes.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FiledRecipes.Views
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RecipeView : ViewBase, IRecipeView
+    {
+        public void Show(IEnumerable<IRecipe> recipes)
+        { }
+        public void Show(IRecipe recipe)
+        { }
+
+    }
+}
