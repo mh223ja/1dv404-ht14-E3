c96f0bc85df84804ad8ca252d0841b154fc3a732
mh223ja <mh223ja@student.lnu.se>
2015-01-08 12:28:56 +0100

    Cleaning up files

---
+++ 1DV404Labs/1DV404-Lab3/ConsoleApplication24/Gymnast.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication24
+{
+    public class Gymnast
+    {
+        string _firstName;
+        string _lastName;
+        int _age;
+        string _gender;
+        string _team;
+        string gymnastInfo;
+     
+      
+
+
+        public string FirstName{
+
+        
+        get {
+        return _firstName;
+        }
+            set
+            {
+                _firstName = FirstName;
+            }
+    }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = LastName;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                _age = Age;
+            }
+        }
+
+        public string Gender
+        {
+            get
+            {
+                return _gender;
+            }
+            set
+            {
+                _gender = Gender;
+            }
+        }
+
+        public string Team
+        {
+            get
+            {
+                return _team;
+            }
+            set
+            {
+                _team = Team;
+            }
+
+        }
+        public Gymnast(string lastName, string firstName, string team, string gender){
+
+        
+            lastName = LastName;
+            firstName = FirstName;
+            team = Team;
+            gender = Gender;
+
+           string[] gymnastArray = new string[]{lastName, firstName, team, gender     
+        };
+         
+        }
+           
+
+    }     
+    
+}
---
+++ 1DV404Labs/1DV404-Lab3/ConsoleApplication24Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApplication24Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApplication24Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d0f2506f-62f8-41b6-a8d8-ed3447b9055b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
